a1c1ccf2cd90e6e5f29a06d3f7f67e37c2924789
Ty Bandachit <tbandachit@gmail.com>
2020-03-18 18:24:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT bears.name, bears.age FROM bears WHERE bears.age == (select max"
+  "SELECT bears.name, bears.age FROM bears WHERE bears.age == (select max(bears.age)"
 end
 
 def select_youngest_bear_and_returns_name_and_age
