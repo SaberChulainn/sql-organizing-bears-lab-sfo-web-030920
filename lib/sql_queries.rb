c1822d34be98af875c7e13bc3bea4f223f88f253
Ty Bandachit <tbandachit@gmail.com>
2020-03-18 18:32:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT bears.name, bears.age FROM bears WHERE bears.age == (select min(bears.age) from bears) AND bears.name is NOT NULL"
 end
 
 def selects_most_prominent_color_and_returns_with_count
