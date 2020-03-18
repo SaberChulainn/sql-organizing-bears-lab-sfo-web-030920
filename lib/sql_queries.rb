bb7848e889e601b54fefdcec7caca459542ced24
Ty Bandachit <tbandachit@gmail.com>
2020-03-18 18:41:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(bears.temperaments) from bears where bears.temperaments == 'goofy'"
+  "SELECT COUNT(bears.temperament) from bears where bears.temperament == 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
