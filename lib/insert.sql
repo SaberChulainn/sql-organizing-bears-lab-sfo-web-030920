6cf8720226f60dbd2803bd12ebcb63ee996c1eef
Ty Bandachit <tbandachit@gmail.com>
2020-03-18 17:57:00 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
-INSERT INTO bears(id, name, age, gender, color, temperament)
-VALUES(1, "Mr.Chocolate", 20, "M", "Black", "Angry"),
-VALUES(2, "Rowdy", 20, "M", "Black", "Angry"),
-VALUES(3, "Tabitha", 20, "F", "Black", "Angry"),
-VALUES(4, "Sergeant Brown", 20, "M", "Black", "Angry"),
-VALUES(5, "Melissa", 20, "F", "Black", "Angry"),
-VALUES(6, "Grinch", 20, "M", "Black", "Angry"),
-VALUES(7, "Wendy", 20, "F", "Black", "Angry"),
-VALUES(8, "Unnamed", 20, "M", "Black", "Angry");
+INSERT INTO bears(name, age, gender, color, temperament)
+VALUES("Mr.Chocolate", 20, "M", "Black", "Angry"),
+VALUES("Rowdy", 20, "M", "Black", "Angry"),
+VALUES("Tabitha", 20, "F", "Black", "Angry"),
+VALUES("Sergeant Brown", 20, "M", "Black", "Angry"),
+VALUES("Melissa", 20, "F", "Black", "Angry"),
+VALUES("Grinch", 20, "M", "Black", "Angry"),
+VALUES("Wendy", 20, "F", "Black", "Angry"),
+VALUES("Unnamed", 20, "M", "Black", "Angry");
